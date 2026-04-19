249b0fabe5081c38e023b016579e30aa0839d1b6
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-19 13:44:37 -0400

    feat: configure security filter chain for local environment

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package io.github.ctorressoftware.academic.enrollment.security.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Profile("local")
+    SecurityFilterChain localFilterChain(HttpSecurity http) {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml")
+                        .permitAll()
+                        .anyRequest().permitAll())
+                .build();
+    }
+}
