186585faaec37fa477ed1f2ab9ab2110c8032d9f
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-20 00:39:23 -0400

    feat: add security filter chain with basic configuration and auth logic

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/config/SecurityConfig.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package io.github.ctorressoftware.academic.enrollment.security.infrastructure.config;
 
+import io.github.ctorressoftware.academic.enrollment.security.infrastructure.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.Clock;
 
 @Configuration
 @EnableMethodSecurity
@@ -31,5 +40,29 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().permitAll())
                 .build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(
+            CustomUserDetailsService customUserDetailsService,
+            PasswordEncoder passwordEncoder) {
+        var provider = new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
     }
 }
