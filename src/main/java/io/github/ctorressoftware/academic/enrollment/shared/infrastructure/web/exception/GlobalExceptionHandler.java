a19a52df4a8b559a782b11bea346de9e39d1989f
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-26 15:10:42 -0400

    feat: adapt global exception handler to new api response implementation

--- src/main/java/io/github/ctorressoftware/academic/enrollment/shared/infrastructure/web/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/shared/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -5,8 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,13 +21,36 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGeneric(Exception e) {
-        log.error("Unhandled exception", e);
+        log.error("Unhandled exception: {}", e.getMessage(), e);
 
         return ResponseEntity
                 .internalServerError()
                 .body(ApiResponse.error(
                         HttpStatus.INTERNAL_SERVER_ERROR.name(),
-                        "Unexpected error")
+                        "Unexpected error",
+                        null)
                 );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, List<String>>>> handleInvalidRequests(MethodArgumentNotValidException e) {
+
+        Map<String, List<String>> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ));
+
+        log.warn("Validation failed. Fields: {}", errors);
+
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(
+                        HttpStatus.BAD_REQUEST.name(),
+                        "Validation failed",
+                        errors
+                ));
     }
 }
