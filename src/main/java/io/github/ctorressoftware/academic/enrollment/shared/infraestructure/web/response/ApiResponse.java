6cc5f0eb4f18c00abcdacdca963a391d3b930458
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-25 23:49:15 -0400

    feat: create generic api response to standardize endpoints responses

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/shared/infraestructure/web/response/ApiResponse.java
@@ -0,0 +1,47 @@
+package io.github.ctorressoftware.academic.enrollment.shared.infraestructure.web.response;
+
+public class ApiResponse<T> {
+    private final boolean success;
+    private final String errorCode;
+    private final String errorMessage;
+    private final T data;
+
+    private ApiResponse(boolean success, String errorCode, String errorMessage, T data) {
+        this.success = success;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(
+                true,
+                null,
+                null,
+                data);
+    }
+
+    public static <T> ApiResponse<T> error(String errorCode, String errorMessage) {
+        return new ApiResponse<>(
+                false,
+                errorCode,
+                errorMessage,
+                null);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
