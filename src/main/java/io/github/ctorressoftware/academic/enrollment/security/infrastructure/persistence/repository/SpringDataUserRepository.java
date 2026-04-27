f613c72aa3d1805e6889e1d0848b22d1a02b67ef
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-27 00:09:51 -0400

    refactor: edit implementation adding method definitions like existsByUsername and findByusername

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/persistence/repository/SpringDataUserRepository.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/persistence/repository/SpringDataUserRepository.java
@@ -1,10 +1,15 @@
 package io.github.ctorressoftware.academic.enrollment.security.infrastructure.persistence.repository;
 
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
 import io.github.ctorressoftware.academic.enrollment.security.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {}
+public interface SpringDataUserRepository extends JpaRepository<UserEntity, UUID> {
+    boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
+}
