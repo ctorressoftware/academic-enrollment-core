6517a34e98b66c3a94b06ce7718af20fc6113e87
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-29 00:57:25 -0400

    feat: add LoginResponse file for controller

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/web/response/LoginResponse.java
@@ -0,0 +1,16 @@
+package io.github.ctorressoftware.academic.enrollment.security.infrastructure.web.response;
+
+import io.github.ctorressoftware.academic.enrollment.security.application.result.LoginResult;
+import io.github.ctorressoftware.academic.enrollment.security.application.result.RegisterUserResult;
+
+public record LoginResponse(
+        String username,
+        String accessToken
+) {
+    public static LoginResponse from(LoginResult result) {
+        return new LoginResponse(
+                result.username(),
+                result.accessToken()
+        );
+    }
+}
