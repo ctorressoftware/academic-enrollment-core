f003a710ef05c90c4a6cf33dc86351f5843a67bf
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-29 00:58:30 -0400

    feat: add LoginService with login bootstrap logic

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/service/LoginService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/service/LoginService.java
@@ -1,12 +1,22 @@
 package io.github.ctorressoftware.academic.enrollment.security.application.service;
 
 import io.github.ctorressoftware.academic.enrollment.security.application.command.LoginCommand;
+import io.github.ctorressoftware.academic.enrollment.security.domain.exception.InvalidCredentialsException;
 import io.github.ctorressoftware.academic.enrollment.security.application.result.LoginResult;
 import io.github.ctorressoftware.academic.enrollment.security.application.usecase.LoginUseCase;
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.PasswordHash;
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.Username;
+import io.github.ctorressoftware.academic.enrollment.security.domain.ports.PasswordHasher;
 import io.github.ctorressoftware.academic.enrollment.security.domain.ports.TokenIssuer;
 import io.github.ctorressoftware.academic.enrollment.security.domain.ports.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LoginService implements LoginUseCase {
@@ -14,18 +24,42 @@ public class LoginService implements LoginUseCase {
     private final UserRepository repository;
     private final AuthenticationManager authManager;
     private final TokenIssuer tokenIssuer;
+    private final PasswordHasher passwordHasher;
 
     public LoginService(
             UserRepository repository,
             AuthenticationManager authManager,
-            TokenIssuer tokenIssuer) {
+            TokenIssuer tokenIssuer,
+            PasswordHasher passwordHasher) {
         this.repository = repository;
         this.authManager = authManager;
         this.tokenIssuer = tokenIssuer;
+        this.passwordHasher = passwordHasher;
     }
 
     @Override
     public LoginResult login(LoginCommand command) {
-        return null;
+
+        Username username = new Username(command.username());
+        PasswordHash passwordHash = passwordHasher.hash(command.password());
+
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new InvalidCredentialsException(username));
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(
+                user,
+                passwordHash.value(),
+                List.of(new SimpleGrantedAuthority("ROLE_STUDENT"))
+        );
+
+        var auth = authManager.authenticate(authToken);
+
+        if (!auth.isAuthenticated()) {
+            throw new InvalidCredentialsException(username);
+        }
+
+        String accessToken = tokenIssuer.issueAccessToken((User) auth.getPrincipal());
+
+        return new LoginResult(username.value(), accessToken);
     }
 }
