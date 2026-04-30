c36b9d23c81f6973839a7bdf78bf005201e671d9
Carlos Torres <ctorressoftware@hotmail.com>
2026-04-30 00:05:03 -0400

    feat: add CustomUserDetailsService to load user from database

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
@@ -1,19 +1,41 @@
 package io.github.ctorressoftware.academic.enrollment.security.infrastructure.security;
 
+import io.github.ctorressoftware.academic.enrollment.security.domain.exception.InvalidCredentialsException;
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
+import io.github.ctorressoftware.academic.enrollment.security.domain.model.Username;
+import io.github.ctorressoftware.academic.enrollment.security.domain.ports.UserRepository;
 import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(@NonNull String usernameValue)
+            throws UsernameNotFoundException {
 
-        AuthUser user = new AuthUser();
+        Username username = new Username(usernameValue);
 
-        return user;
+        User user = repository.findByUsername(username).orElseThrow(() ->
+                        new InvalidCredentialsException(username));
+
+        return new AuthUser(
+                user.getUsername().value(),
+                user.getPasswordHash().value(),
+                List.of(new SimpleGrantedAuthority("ROLE_STUDENT")),
+                user.isActive()
+        );
     }
 }
