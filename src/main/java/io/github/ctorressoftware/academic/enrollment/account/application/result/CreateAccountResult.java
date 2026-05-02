7ed6ff0f051b52d51356a51c06aacd4f162d8521
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-02 16:51:44 -0400

    feat: add CreateAccountResult for use case

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/account/application/result/CreateAccountResult.java
@@ -0,0 +1,9 @@
+package io.github.ctorressoftware.academic.enrollment.account.application.result;
+
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Person;
+
+public record CreateAccountResult(
+        Person person,
+        String username,
+        String accessToken
+) {}
