88006a11f3e5edf05feb073722bf52bcb683e2a9
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-01 22:52:09 -0400

    feat: add jpa person repository adapter

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/infrastructure/persistence/adapter/JpaPersonRepositoryAdapter.java
@@ -0,0 +1,62 @@
+package io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.adapter;
+
+import io.github.ctorressoftware.academic.enrollment.person.application.port.out.PersonRepository;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Document;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.DocumentType;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Email;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Person;
+import io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.entity.PersonEntity;
+import io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.repository.SpringDataPersonRepository;
+import org.springframework.stereotype.Component;
+
+import static io.github.ctorressoftware.academic.enrollment.person.domain.model.DocumentType.*;
+
+@Component
+public class JpaPersonRepositoryAdapter implements PersonRepository {
+
+    private final SpringDataPersonRepository repository;
+
+    public JpaPersonRepositoryAdapter(SpringDataPersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Person save(Person person) {
+        PersonEntity saved = repository.save(toEntity(person));
+        return toDomain(saved);
+    }
+
+    private Person toDomain(PersonEntity entity) {
+        DocumentType type = entity.getDocumentTypeId() == DNI.getId()
+                ? DNI : PASSPORT;
+
+        Document document = new Document(type, entity.getDocumentNumber());
+
+        return Person.create(
+                entity.getFirstName(),
+                entity.getMiddleName(),
+                entity.getLastName(),
+                entity.getSecondLastName(),
+                document,
+                entity.getGenderId(),
+                new Email(entity.getEmail())
+        );
+    }
+
+    private PersonEntity toEntity(Person person) {
+        PersonEntity entity = new PersonEntity();
+        entity.setId(person.getId());
+        entity.setFirstName(person.getFirstName());
+        entity.setMiddleName(person.getMiddleName());
+        entity.setLastName(person.getLastName());
+        entity.setSecondLastName(person.getSecondLastName());
+        entity.setDocumentTypeId(person.getDocument().type().getId());
+        entity.setDocumentNumber(person.getDocument().value());
+        entity.setGenderId(person.getGenderId());
+        entity.setEmail(person.getEmail().value());
+        entity.setActive(person.isActive());
+        entity.setCreatedAt(person.getCreatedAt());
+        entity.setUpdatedAt(person.getUpdatedAt());
+        return entity;
+    }
+}
