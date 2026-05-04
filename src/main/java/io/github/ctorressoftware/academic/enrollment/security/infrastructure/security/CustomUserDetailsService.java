f16370a0e49a56a67ee92b4ebb777318d28c71f0
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 13:43:12 -0400

    feat: implement UserDetailsLoader interface and implement loadUserById method

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package io.github.ctorressoftware.academic.enrollment.security.infrastructure.security;
 
+import io.github.ctorressoftware.academic.enrollment.security.application.port.out.UserDetailsLoader;
 import io.github.ctorressoftware.academic.enrollment.security.domain.exception.InvalidCredentialsException;
 import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
 import io.github.ctorressoftware.academic.enrollment.security.domain.model.Username;
@@ -7,14 +8,15 @@ import io.github.ctorressoftware.academic.enrollment.security.application.port.o
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsLoader {
 
     private final UserRepository repository;
 
@@ -30,6 +32,22 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         User user = repository.findByUsername(username).orElseThrow(() ->
                         new InvalidCredentialsException(username));
+
+        return new AuthUser(
+                user.getUsername().value(),
+                user.getPasswordHash().value(),
+                List.of(new SimpleGrantedAuthority("ROLE_STUDENT")),
+                user.isActive()
+        );
+    }
+
+    public UserDetails loadUserById(@NonNull String userId)
+            throws UsernameNotFoundException {
+
+        UUID id = UUID.fromString(Objects.requireNonNull(userId));
+
+        User user = repository.findById(id)
+                .orElseThrow(() -> new InvalidCredentialsException(id));
 
         return new AuthUser(
                 user.getUsername().value(),
