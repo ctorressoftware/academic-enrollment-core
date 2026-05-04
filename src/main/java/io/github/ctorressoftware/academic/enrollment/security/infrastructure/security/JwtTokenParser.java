256dfd27c0955f59d84f1ba2865c1b32c3d00828
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 01:13:16 -0400

    feat: add token parser implementation

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/JwtTokenParser.java
@@ -0,0 +1,29 @@
+package io.github.ctorressoftware.academic.enrollment.security.infrastructure.security;
+
+import io.github.ctorressoftware.academic.enrollment.security.application.port.out.TokenParser;
+import io.github.ctorressoftware.academic.enrollment.security.infrastructure.config.JwtConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
+public class JwtTokenParser implements TokenParser {
+
+    private final SecretKey secretKey;
+
+    public JwtTokenParser(JwtConfig config) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(config.getSecretKey()));
+    }
+
+    @Override
+    public Jws<Claims> parse(String jwt) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(jwt);
+    }
+}
+
