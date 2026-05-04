d67c3c1cd32ddaa7b40384322d627646476387e9
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 13:26:16 -0400

    feat: add findById to UserRepository to retrieve user data by uuid pk

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/port/out/UserRepository.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/port/out/UserRepository.java
@@ -4,9 +4,11 @@ import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
 import io.github.ctorressoftware.academic.enrollment.security.domain.model.Username;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository {
     boolean existsByUsername(Username username);
     Optional<User> findByUsername(Username username);
+    Optional<User> findById(UUID userId);
     User save(User user);
 }
