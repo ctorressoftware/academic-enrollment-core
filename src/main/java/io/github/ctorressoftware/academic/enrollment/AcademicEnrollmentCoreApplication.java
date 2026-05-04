3eb40f378127f7a9f7f012a64d05133cd79fa494
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 19:03:40 -0400

    chore: add todo comment

--- src/main/java/io/github/ctorressoftware/academic/enrollment/AcademicEnrollmentCoreApplication.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/AcademicEnrollmentCoreApplication.java
@@ -3,6 +3,7 @@ package io.github.ctorressoftware.academic.enrollment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: create code parameter to custom exceptions for GlobalExceptionHandler
 @SpringBootApplication
 public class AcademicEnrollmentCoreApplication {
 
