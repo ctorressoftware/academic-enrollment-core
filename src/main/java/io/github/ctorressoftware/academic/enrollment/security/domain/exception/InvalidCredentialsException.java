e797814ee9b01c55e24a5eb34738477fdbe1296e
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 13:25:25 -0400

    feat: overload the constructor to support userId parameter

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/domain/exception/InvalidCredentialsException.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/domain/exception/InvalidCredentialsException.java
@@ -2,8 +2,14 @@ package io.github.ctorressoftware.academic.enrollment.security.domain.exception;
 
 import io.github.ctorressoftware.academic.enrollment.security.domain.model.Username;
 
+import java.util.UUID;
+
 public class InvalidCredentialsException extends RuntimeException {
     public InvalidCredentialsException(Username username) {
         super("Invalid credentials for user: " + username.value());
+    }
+
+    public InvalidCredentialsException(UUID userId) {
+        super("Invalid credentials for user id: " + userId.toString());
     }
 }
