43813b85ef4a4332a4eae4f9dc4df39b03dd1b81
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 22:09:45 -0400

    feat: add CreateStudentResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/student/application/port/in/create/CreateStudentResult.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.student.application.port.in.create;
+
+import io.github.ctorressoftware.academic.enrollment.student.domain.model.Student;
+
+public record CreateStudentResult(Student student) {}
