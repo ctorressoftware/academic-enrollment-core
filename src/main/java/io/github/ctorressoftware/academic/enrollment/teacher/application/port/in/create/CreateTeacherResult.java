b6f46c322c8f105442f15b5d356bba770153925d
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-04 22:32:47 -0400

    feat: add CreateTeacherResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/teacher/application/port/in/create/CreateTeacherResult.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.teacher.application.port.in.create;
+
+import io.github.ctorressoftware.academic.enrollment.teacher.domain.model.Teacher;
+
+public record CreateTeacherResult(Teacher teacher) {}
