dffdcc400cecddbfa9af994604a014dd6ffbd0aa
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-06 01:15:10 -0400

    feat: create CreateTeacherAccountService implementing usecase file

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/account/application/service/CreateTeacherAccountService.java
@@ -0,0 +1,86 @@
+package io.github.ctorressoftware.academic.enrollment.account.application.service;
+
+import io.github.ctorressoftware.academic.enrollment.account.application.port.in.create.teacher.CreateTeacherAccountCommand;
+import io.github.ctorressoftware.academic.enrollment.account.application.port.in.create.teacher.CreateTeacherAccountResult;
+import io.github.ctorressoftware.academic.enrollment.account.application.port.in.create.teacher.CreateTeacherAccountUseCase;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.create.CreatePersonCommand;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.create.CreatePersonResult;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.create.CreatePersonUseCase;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Document;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.DocumentType;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.Email;
+import io.github.ctorressoftware.academic.enrollment.security.application.port.in.register.RegisterUserCommand;
+import io.github.ctorressoftware.academic.enrollment.security.application.port.in.register.RegisterUserResult;
+import io.github.ctorressoftware.academic.enrollment.security.application.port.in.register.RegisterUserUseCase;
+import io.github.ctorressoftware.academic.enrollment.shared.application.port.out.UnitOfWork;
+import io.github.ctorressoftware.academic.enrollment.teacher.application.port.in.create.CreateTeacherCommand;
+import io.github.ctorressoftware.academic.enrollment.teacher.application.port.in.create.CreateTeacherResult;
+import io.github.ctorressoftware.academic.enrollment.teacher.application.port.in.create.CreateTeacherUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CreateTeacherAccountService implements CreateTeacherAccountUseCase {
+    private final CreatePersonUseCase createPersonUseCase;
+    private final CreateTeacherUseCase teacherUseCase;
+    private final RegisterUserUseCase registerUserUseCase;
+    private final UnitOfWork unitOfWork;
+
+    public CreateTeacherAccountService(
+            CreatePersonUseCase createPersonUseCase,
+            CreateTeacherUseCase teacherUseCase,
+            RegisterUserUseCase registerUserUseCase,
+            UnitOfWork unitOfWork) {
+        this.createPersonUseCase = createPersonUseCase;
+        this.teacherUseCase = teacherUseCase;
+        this.registerUserUseCase = registerUserUseCase;
+        this.unitOfWork = unitOfWork;
+    }
+
+    @Override
+    @Transactional
+    public CreateTeacherAccountResult create(CreateTeacherAccountCommand command) {
+
+        Document document = new Document(
+                DocumentType.getById(command.documentTypeId()),
+                command.documentNumber()
+        );
+
+        CreatePersonCommand personCommand = new CreatePersonCommand(
+                command.firstName(),
+                command.middleName(),
+                command.lastName(),
+                command.secondLastName(),
+                document,
+                command.genderId(),
+                new Email(command.email())
+        );
+
+        CreatePersonResult createPersonResult =
+                createPersonUseCase.create(personCommand);
+
+        unitOfWork.flush();
+
+        CreateTeacherCommand createTeacherCommand =
+                new CreateTeacherCommand(createPersonResult.person().getId());
+
+        CreateTeacherResult createTeacherResult =
+                teacherUseCase.create(createTeacherCommand);
+
+        RegisterUserCommand registerCommand = new RegisterUserCommand(
+                createPersonResult.person().getId(),
+                command.username(),
+                command.password()
+        );
+
+        RegisterUserResult registerUserResult =
+                registerUserUseCase.register(registerCommand);
+
+        return new CreateTeacherAccountResult(
+                createPersonResult.person(),
+                createTeacherResult.teacher(),
+                registerUserResult.username(),
+                registerUserResult.accessToken()
+        );
+    }
+}
