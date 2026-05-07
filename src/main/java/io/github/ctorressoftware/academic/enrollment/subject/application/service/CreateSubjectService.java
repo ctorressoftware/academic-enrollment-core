093ea3b807e80c2d0e9d5e46313c63fa22cb8a4e
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-07 01:58:20 -0400

    feat: implement create subject service

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/subject/application/service/CreateSubjectService.java
@@ -0,0 +1,29 @@
+package io.github.ctorressoftware.academic.enrollment.subject.application.service;
+
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.create.CreateSubjectCommand;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.create.CreateSubjectResult;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.create.CreateSubjectUseCase;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.out.SubjectRepository;
+import io.github.ctorressoftware.academic.enrollment.subject.domain.model.Subject;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class CreateSubjectService implements CreateSubjectUseCase {
+
+    private final SubjectRepository repository;
+
+    public CreateSubjectService(SubjectRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public CreateSubjectResult create(CreateSubjectCommand command) {
+        Objects.requireNonNull(command, "command is null");
+        Subject subject = Subject.create(command.code(), command.description());
+        Subject saved = repository.save(subject);
+
+        return new CreateSubjectResult(saved);
+    }
+}
