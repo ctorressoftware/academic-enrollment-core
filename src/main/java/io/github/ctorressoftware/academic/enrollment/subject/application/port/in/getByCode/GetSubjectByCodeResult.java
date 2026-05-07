0d2985ad8c556909db738524bebb3c9f2d2e3ae1
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-07 01:57:16 -0400

    feat: add GetSubjectByCodeResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/subject/application/port/in/getByCode/GetSubjectByCodeResult.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.subject.application.port.in.getByCode;
+
+import io.github.ctorressoftware.academic.enrollment.subject.domain.model.Subject;
+
+public record GetSubjectByCodeResult(Subject subject) {}
