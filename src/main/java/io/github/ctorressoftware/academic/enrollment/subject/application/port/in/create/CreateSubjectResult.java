48ab449a867cebca44fa9529d05b6ae0047f1e18
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-07 01:54:30 -0400

    feat: add CreateSubjectResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/subject/application/port/in/create/CreateSubjectResult.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.subject.application.port.in.create;
+
+import io.github.ctorressoftware.academic.enrollment.subject.domain.model.Subject;
+
+public record CreateSubjectResult(Subject subject) {}
