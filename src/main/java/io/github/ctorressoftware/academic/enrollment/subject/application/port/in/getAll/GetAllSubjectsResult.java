0f196f8a02331fbc56952fb04d3498b63f9d6b6d
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-07 01:56:06 -0400

    feat: add GetAllSubjectsResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/subject/application/port/in/getAll/GetAllSubjectsResult.java
@@ -0,0 +1,7 @@
+package io.github.ctorressoftware.academic.enrollment.subject.application.port.in.getAll;
+
+import io.github.ctorressoftware.academic.enrollment.subject.domain.model.Subject;
+
+import java.util.List;
+
+public record GetAllSubjectsResult(List<Subject> subjects) {}
