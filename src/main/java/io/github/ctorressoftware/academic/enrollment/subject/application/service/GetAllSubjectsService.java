414677f7b9f718446b87e483d6c485960ecd7ec3
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-07 01:58:47 -0400

    feat: implement get all subjects with pagination service

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/subject/application/service/GetAllSubjectsService.java
@@ -0,0 +1,31 @@
+package io.github.ctorressoftware.academic.enrollment.subject.application.service;
+
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.getAll.GetAllSubjectsCommand;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.getAll.GetAllSubjectsResult;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.in.getAll.GetAllSubjectsUseCase;
+import io.github.ctorressoftware.academic.enrollment.subject.application.port.out.SubjectRepository;
+import io.github.ctorressoftware.academic.enrollment.subject.domain.model.Subject;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class GetAllSubjectsService implements GetAllSubjectsUseCase {
+
+    private final SubjectRepository repository;
+
+    public GetAllSubjectsService(SubjectRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public GetAllSubjectsResult getAll(GetAllSubjectsCommand command) {
+        Objects.requireNonNull(command, "command is null");
+
+        List<Subject> subjects = repository
+                .findAll(command.page(), command.pageSize());
+
+        return new GetAllSubjectsResult(subjects);
+    }
+}
