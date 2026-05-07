419995949cc88c420b50db9a4b95eb4d5d158f85
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-06 22:23:39 -0400

    fix: adjust UserDetailsService import

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/port/out/UserDetailsLoader.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/application/port/out/UserDetailsLoader.java
@@ -1,8 +1,9 @@
 package io.github.ctorressoftware.academic.enrollment.security.application.port.out;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserDetailsLoader extends org.springframework.security.core.userdetails.UserDetailsService {
+public interface UserDetailsLoader extends UserDetailsService {
     UserDetails loadUserById(String userId) throws UsernameNotFoundException;
 }
