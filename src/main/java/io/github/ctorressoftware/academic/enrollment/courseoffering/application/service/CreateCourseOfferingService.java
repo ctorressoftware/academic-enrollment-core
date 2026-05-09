703b737069df9fb1b6b60bc04b6f59790c23b2d1
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-08 23:48:03 -0400

    feat: add CreateCourseOfferingService

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/courseoffering/application/service/CreateCourseOfferingService.java
@@ -0,0 +1,35 @@
+package io.github.ctorressoftware.academic.enrollment.courseoffering.application.service;
+
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.create.CreateCourseOfferingCommand;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.create.CreateCourseOfferingResult;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.create.CreateCourseOfferingUseCase;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.out.CourseOfferingRepository;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.domain.model.CourseOffering;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCourseOfferingService implements CreateCourseOfferingUseCase {
+
+    private final CourseOfferingRepository repository;
+
+    public CreateCourseOfferingService(CourseOfferingRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public CreateCourseOfferingResult create(CreateCourseOfferingCommand command) {
+
+        CourseOffering courseOffering = CourseOffering.create(
+                command.groupCode(),
+                command.academicPeriodId(),
+                command.subjectId(),
+                command.teacherId(),
+                command.courseStateId(),
+                command.quotas()
+        );
+
+        CourseOffering saved = repository.save(courseOffering);
+
+        return new CreateCourseOfferingResult(saved);
+    }
+}
