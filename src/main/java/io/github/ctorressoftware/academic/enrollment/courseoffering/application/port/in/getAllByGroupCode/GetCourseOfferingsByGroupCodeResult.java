37541b835f371fe9569ee47e7c8fcdcfd542a00c
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-08 23:43:19 -0400

    feat: add GetCourseOfferingsByGroupCodeResult

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/courseoffering/application/port/in/getAllByGroupCode/GetCourseOfferingsByGroupCodeResult.java
@@ -0,0 +1,7 @@
+package io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.getAllByGroupCode;
+
+import io.github.ctorressoftware.academic.enrollment.courseoffering.domain.model.CourseOffering;
+
+import java.util.List;
+
+public record GetCourseOfferingsByGroupCodeResult(List<CourseOffering> courseOfferings) {}
