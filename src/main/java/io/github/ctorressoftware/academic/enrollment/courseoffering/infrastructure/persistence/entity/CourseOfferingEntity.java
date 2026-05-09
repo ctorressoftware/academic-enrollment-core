ff8c0296644a786327f021f15337616437b17eb7
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-08 23:44:17 -0400

    feat: add course offering entity

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/courseoffering/infrastructure/persistence/entity/CourseOfferingEntity.java
@@ -0,0 +1,112 @@
+package io.github.ctorressoftware.academic.enrollment.courseoffering.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "course_offering", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"group_code", "academic_period_id"})
+})
+public class CourseOfferingEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "group_code", nullable = false)
+    private String groupCode;
+
+    @Column(name = "academic_period_id")
+    private UUID academicPeriodId;
+
+    @Column(name = "subject_id")
+    private UUID subjectId;
+
+    @Column(name = "teacher_id")
+    private UUID teacherId;
+
+    @Column(name = "course_state_id")
+    private short courseStateId;
+
+    @Column(name = "quotas")
+    private short quotas;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getGroupCode() {
+        return groupCode;
+    }
+
+    public void setGroupCode(String groupCode) {
+        this.groupCode = groupCode;
+    }
+
+    public UUID getAcademicPeriodId() {
+        return academicPeriodId;
+    }
+
+    public void setAcademicPeriodId(UUID academicPeriodId) {
+        this.academicPeriodId = academicPeriodId;
+    }
+
+    public UUID getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(UUID subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public UUID getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(UUID teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public short getCourseStateId() {
+        return courseStateId;
+    }
+
+    public void setCourseStateId(short courseStateId) {
+        this.courseStateId = courseStateId;
+    }
+
+    public short getQuotas() {
+        return quotas;
+    }
+
+    public void setQuotas(short quotas) {
+        this.quotas = quotas;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
