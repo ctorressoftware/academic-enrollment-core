4462af40f04bb2d19183086eacf7a3adc9f0e270
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-08 23:48:23 -0400

    feat: add GetCourseOfferingsByGroupCodeService

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/courseoffering/application/service/GetCourseOfferingsByGroupCodeService.java
@@ -0,0 +1,31 @@
+package io.github.ctorressoftware.academic.enrollment.courseoffering.application.service;
+
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.getAllByGroupCode.GetCourseOfferingsByGroupCodeCommand;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.getAllByGroupCode.GetCourseOfferingsByGroupCodeResult;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.in.getAllByGroupCode.GetCourseOfferingsByGroupCodeUseCase;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.out.CourseOfferingRepository;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.domain.model.CourseOffering;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GetCourseOfferingsByGroupCodeService
+        implements GetCourseOfferingsByGroupCodeUseCase {
+
+    private final CourseOfferingRepository repository;
+
+    public GetCourseOfferingsByGroupCodeService(
+            CourseOfferingRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public GetCourseOfferingsByGroupCodeResult getAll(GetCourseOfferingsByGroupCodeCommand command) {
+
+        List<CourseOffering> courseOfferings =
+                repository.getAllByGroupCode(command.groupCode());
+
+        return new GetCourseOfferingsByGroupCodeResult(courseOfferings);
+    }
+}
