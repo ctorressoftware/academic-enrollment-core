b2a1e623ef452a04ea2abaa1ec368e9cf2703c84
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-10 23:46:35 -0400

    feat: add createEnrollmentResponse file

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/enrollment/infrastructure/web/response/CreateEnrollmentResponse.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.enrollment.infrastructure.web.response;
+
+import io.github.ctorressoftware.academic.enrollment.enrollment.domain.model.Enrollment;
+
+public record CreateEnrollmentResponse(Enrollment enrollment) {}
