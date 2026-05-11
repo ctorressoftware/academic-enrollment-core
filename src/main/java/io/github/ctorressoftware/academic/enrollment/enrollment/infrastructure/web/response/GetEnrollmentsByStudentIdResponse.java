b8f2f281c7ed26988843b14f45835b29f6581992
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-10 23:47:00 -0400

    feat: add GetEnrollmentsByStudentIdResponse file

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/enrollment/infrastructure/web/response/GetEnrollmentsByStudentIdResponse.java
@@ -0,0 +1,9 @@
+package io.github.ctorressoftware.academic.enrollment.enrollment.infrastructure.web.response;
+
+import io.github.ctorressoftware.academic.enrollment.enrollment.domain.model.Enrollment;
+
+import java.util.List;
+
+public record GetEnrollmentsByStudentIdResponse(
+        List<Enrollment> enrollments
+) {}
