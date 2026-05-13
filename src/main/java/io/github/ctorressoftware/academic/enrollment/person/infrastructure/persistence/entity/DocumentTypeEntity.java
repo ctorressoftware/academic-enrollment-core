6cb1d0e9c6c3c997029d2104c22c21eb84cfa6a6
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-12 22:58:13 -0400

    feat: add DocumentType entity

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/infrastructure/persistence/entity/DocumentTypeEntity.java
@@ -0,0 +1,90 @@
+package io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity(name = "document_type")
+public class DocumentTypeEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Short id;
+
+    @Size(max = 20)
+    @NotBlank(message = "code cannot be blank")
+    @Column(name = "code", nullable = false, length = 20)
+    private String code;
+
+    @Size(max = 100)
+    @NotBlank(message = "description cannot be blank")
+    @Column(name = "description", nullable = false, length = 100)
+    private String description;
+
+    @NotNull
+    @ColumnDefault("true")
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @NotNull
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+}
