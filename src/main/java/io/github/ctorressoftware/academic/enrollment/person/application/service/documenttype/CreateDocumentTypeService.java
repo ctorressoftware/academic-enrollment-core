517d58d4184d00b08b32e7a88f9cf5b479164ccc
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-12 23:00:21 -0400

    feat: add CreateDocumentType service

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/application/service/documenttype/CreateDocumentTypeService.java
@@ -0,0 +1,30 @@
+package io.github.ctorressoftware.academic.enrollment.person.application.service.documenttype;
+
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.documenttype.create.CreateDocumentTypeCommand;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.documenttype.create.CreateDocumentTypeResult;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.in.documenttype.create.CreateDocumentTypeUseCase;
+import io.github.ctorressoftware.academic.enrollment.person.application.port.out.DocumentTypeRepository;
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.documenttype.DocumentType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateDocumentTypeService implements CreateDocumentTypeUseCase {
+
+    private final DocumentTypeRepository repository;
+
+    public CreateDocumentTypeService(DocumentTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public CreateDocumentTypeResult save(CreateDocumentTypeCommand command) {
+        DocumentType documentType = DocumentType.create(
+                command.id(),
+                command.code(),
+                command.description()
+        );
+
+        DocumentType saved = repository.save(documentType);
+
+        return new CreateDocumentTypeResult(saved);    }
+}
