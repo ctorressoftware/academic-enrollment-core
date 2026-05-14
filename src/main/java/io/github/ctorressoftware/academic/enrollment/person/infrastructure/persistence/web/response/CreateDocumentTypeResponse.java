89ee3bb45d59da74f0059bb6e53db3bf73415925
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-13 23:04:38 -0400

    feat: add document type and gender create responses

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/infrastructure/persistence/web/response/CreateDocumentTypeResponse.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.web.response;
+
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.documenttype.DocumentType;
+
+public record CreateDocumentTypeResponse(DocumentType documentType) {}
---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/infrastructure/persistence/web/response/CreateGenderResponse.java
@@ -0,0 +1,5 @@
+package io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.web.response;
+
+import io.github.ctorressoftware.academic.enrollment.person.domain.model.gender.Gender;
+
+public record CreateGenderResponse(Gender gender) {}
