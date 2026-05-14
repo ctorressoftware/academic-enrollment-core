291229ce2f4f9be94bd16ab982625410dd9547a4
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-13 23:04:12 -0400

    feat: add document type and gender create requests

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/person/infrastructure/persistence/web/request/CreateDocumentTypeRequest.java
@@ -0,0 +1,15 @@
+package io.github.ctorressoftware.academic.enrollment.person.infrastructure.persistence.web.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateDocumentTypeRequest(
+        @Min(value = 1, message = "id has to be higher than zero")
+        short id,
+
+        @NotBlank(message = "code cannot be blank")
+        String code,
+
+        @NotBlank(message = "description cannot be blank")
+        String description
+) {}
