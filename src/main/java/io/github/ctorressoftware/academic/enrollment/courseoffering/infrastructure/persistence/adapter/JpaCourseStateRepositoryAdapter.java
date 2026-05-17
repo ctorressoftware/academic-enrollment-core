da65c4801f338bfb3f0f62c3117583806d174c43
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-17 14:55:13 -0400

    feat: add CourseState repository adapter

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/courseoffering/infrastructure/persistence/adapter/JpaCourseStateRepositoryAdapter.java
@@ -0,0 +1,56 @@
+package io.github.ctorressoftware.academic.enrollment.courseoffering.infrastructure.persistence.adapter;
+
+import io.github.ctorressoftware.academic.enrollment.courseoffering.application.port.out.CourseStateRepository;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.domain.model.CourseState;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.infrastructure.persistence.entity.CourseStateEntity;
+import io.github.ctorressoftware.academic.enrollment.courseoffering.infrastructure.persistence.repository.SpringDataCourseStateRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JpaCourseStateRepositoryAdapter implements CourseStateRepository {
+
+    private final SpringDataCourseStateRepository repository;
+
+    public JpaCourseStateRepositoryAdapter(
+            SpringDataCourseStateRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public CourseState save(CourseState state) {
+        CourseStateEntity saved = repository.save(toEntity(state));
+        return toDomain(saved);
+    }
+
+    @Override
+    public List<CourseState> getAll() {
+        return repository.findAll().stream()
+                .map(this::toDomain)
+                .toList();
+    }
+
+    private CourseState toDomain(CourseStateEntity entity) {
+
+        return CourseState.restore(
+                entity.getId(),
+                entity.getCode(),
+                entity.getDescription(),
+                entity.getActive(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+    }
+
+    private CourseStateEntity toEntity(CourseState state) {
+        CourseStateEntity entity = new CourseStateEntity();
+        entity.setId(state.getId());
+        entity.setCode(state.getCode());
+        entity.setDescription(state.getDescription());
+        entity.setActive(state.isActive());
+        entity.setCreatedAt(state.getCreatedAt());
+        entity.setUpdatedAt(state.getUpdatedAt());
+        return entity;
+    }
+}
