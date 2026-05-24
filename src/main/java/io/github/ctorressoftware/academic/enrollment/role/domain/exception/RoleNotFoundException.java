b9c6d4d75db84aba01ab8bb0540f12fe38b6a8b5
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-24 19:14:07 -0400

    feat: overload RoleNotFoundException constructor

--- src/main/java/io/github/ctorressoftware/academic/enrollment/role/domain/exception/RoleNotFoundException.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/role/domain/exception/RoleNotFoundException.java
@@ -1,6 +1,11 @@
 package io.github.ctorressoftware.academic.enrollment.role.domain.exception;
 
 public class RoleNotFoundException extends RuntimeException {
+
+    public RoleNotFoundException(short id) {
+        super("Role not found with id: " + id);
+    }
+
     public RoleNotFoundException(String code) {
         super("Role not found with code: " + code);
     }
