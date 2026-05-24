172deaf4361e989638c031d54178b60638db109a
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-24 19:15:43 -0400

    feat: get role from database and set in authuser to manage roles properly

--- src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/security/infrastructure/security/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package io.github.ctorressoftware.academic.enrollment.security.infrastructure.security;
 
+import io.github.ctorressoftware.academic.enrollment.role.application.port.out.RoleRepository;
+import io.github.ctorressoftware.academic.enrollment.role.domain.exception.RoleNotFoundException;
+import io.github.ctorressoftware.academic.enrollment.role.domain.model.Role;
 import io.github.ctorressoftware.academic.enrollment.security.application.port.out.UserDetailsLoader;
 import io.github.ctorressoftware.academic.enrollment.security.domain.exception.InvalidCredentialsException;
 import io.github.ctorressoftware.academic.enrollment.security.domain.model.User;
@@ -18,10 +21,14 @@ import java.util.UUID;
 @Service
 public class CustomUserDetailsService implements UserDetailsLoader {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public CustomUserDetailsService(UserRepository repository) {
-        this.repository = repository;
+    public CustomUserDetailsService(
+            UserRepository userRepository,
+            RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -30,13 +37,16 @@ public class CustomUserDetailsService implements UserDetailsLoader {
 
         Username username = new Username(usernameValue);
 
-        User user = repository.findByUsername(username).orElseThrow(() ->
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
                         new InvalidCredentialsException(username));
+
+        Role role = roleRepository.findById(user.getRoleId())
+                .orElseThrow(() -> new RoleNotFoundException(user.getRoleId()));
 
         return new AuthUser(
                 user.getUsername().value(),
                 user.getPasswordHash().value(),
-                List.of(new SimpleGrantedAuthority("ROLE_STUDENT")),
+                List.of(new SimpleGrantedAuthority("ROLE_" + role.getCode())),
                 user.isActive()
         );
     }
@@ -46,13 +56,16 @@ public class CustomUserDetailsService implements UserDetailsLoader {
 
         UUID id = UUID.fromString(Objects.requireNonNull(userId));
 
-        User user = repository.findById(id)
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new InvalidCredentialsException(id));
+
+        Role role = roleRepository.findById(user.getRoleId())
+                .orElseThrow(() -> new RoleNotFoundException(user.getRoleId()));
 
         return new AuthUser(
                 user.getUsername().value(),
                 user.getPasswordHash().value(),
-                List.of(new SimpleGrantedAuthority("ROLE_STUDENT")),
+                List.of(new SimpleGrantedAuthority("ROLE_" + role.getCode())),
                 user.isActive()
         );
     }
