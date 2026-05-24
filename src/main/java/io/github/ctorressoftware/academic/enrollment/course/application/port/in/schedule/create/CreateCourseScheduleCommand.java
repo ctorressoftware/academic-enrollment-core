ad3534293b29a094137c7e34c20999a5a4b47416
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-24 13:54:19 -0400

    fix: remove problematic import

--- src/main/java/io/github/ctorressoftware/academic/enrollment/course/application/port/in/schedule/create/CreateCourseScheduleCommand.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/course/application/port/in/schedule/create/CreateCourseScheduleCommand.java
@@ -1,7 +1,5 @@
 package io.github.ctorressoftware.academic.enrollment.course.application.port.in.schedule.create;
 
-import io.github.ctorressoftware.academic.enrollment.course.domain.model.WeekDay;
-
 import java.time.LocalTime;
 import java.util.UUID;
 
