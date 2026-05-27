4b557e23df62da39f9e610987b52d57fff5e4e41
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-26 23:51:10 -0400

    feat: add calendar repositories adapters

---
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/infrastructure/persistence/adapter/JpaEnrollmentWindowRepositoryAdapter.java
@@ -0,0 +1,7 @@
+package io.github.ctorressoftware.academic.enrollment.calendar.infrastructure.persistence.adapter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JpaEnrollmentWindowRepositoryAdapter {
+}
