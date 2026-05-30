db1228b034b79d37257b5b335b4a28734b088f10
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-29 23:26:22 -0400

    fix: adjust implemented class in both calendar repository implementations and add method

--- src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/infrastructure/persistence/repository/SpringDataAcademicPeriodRepository.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/infrastructure/persistence/repository/SpringDataAcademicPeriodRepository.java
@@ -1,13 +1,15 @@
 package io.github.ctorressoftware.academic.enrollment.calendar.infrastructure.persistence.repository;
 
-import io.github.ctorressoftware.academic.enrollment.calendar.domain.model.AcademicPeriod;
+import io.github.ctorressoftware.academic.enrollment.calendar.infrastructure.persistence.entity.AcademicPeriodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SpringDataAcademicPeriodRepository
-        extends JpaRepository<AcademicPeriod, UUID> {
+        extends JpaRepository<AcademicPeriodEntity, UUID> {
 
+    Optional<AcademicPeriodEntity> findByQuarterAndYearAndActiveIsTrue(String quarter, Short year);
 }
--- src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/infrastructure/persistence/repository/SpringDataEnrollmentWindowRepository.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/infrastructure/persistence/repository/SpringDataEnrollmentWindowRepository.java
@@ -1,13 +1,15 @@
 package io.github.ctorressoftware.academic.enrollment.calendar.infrastructure.persistence.repository;
 
-import io.github.ctorressoftware.academic.enrollment.calendar.domain.model.EnrollmentWindow;
+import io.github.ctorressoftware.academic.enrollment.calendar.infrastructure.persistence.entity.EnrollmentWindowEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SpringDataEnrollmentWindowRepository
-        extends JpaRepository<EnrollmentWindow, UUID> {
+        extends JpaRepository<EnrollmentWindowEntity, UUID> {
 
+    Optional<EnrollmentWindowEntity> findByAcademicPeriodIdAndActiveIsTrue(UUID academicPeriodId);
 }
