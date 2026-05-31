567506ea325d471322ab1a917e9b8890ad71f508
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-30 23:53:19 -0400

    feat: add academic period services

--- src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/service/academicperiod/CreateAcademicPeriodService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/service/academicperiod/CreateAcademicPeriodService.java
@@ -1,4 +1,33 @@
 package io.github.ctorressoftware.academic.enrollment.calendar.application.service.academicperiod;
 
-public class CreateAcademicPeriodService {
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.create.CreateAcademicPeriodCommand;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.create.CreateAcademicPeriodResult;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.create.CreateAcademicPeriodUseCase;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.out.AcademicPeriodRepository;
+import io.github.ctorressoftware.academic.enrollment.calendar.domain.model.AcademicPeriod;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateAcademicPeriodService implements CreateAcademicPeriodUseCase {
+
+    private final AcademicPeriodRepository academicPeriodRepository;
+
+    public CreateAcademicPeriodService(AcademicPeriodRepository academicPeriodRepository) {
+        this.academicPeriodRepository = academicPeriodRepository;
+    }
+
+    @Override
+    public CreateAcademicPeriodResult create(CreateAcademicPeriodCommand command) {
+        academicPeriodRepository
+                .findActiveByQuarterAndYear(command.quarter(), command.year())
+                .ifPresent(academicPeriod -> {
+                    throw new IllegalArgumentException("Academic period already exists");
+                });
+
+        AcademicPeriod academicPeriod = AcademicPeriod.create(command.quarter(), command.year());
+
+        AcademicPeriod saved = academicPeriodRepository.save(academicPeriod);
+
+        return new CreateAcademicPeriodResult(saved);
+    }
 }
--- src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/service/academicperiod/FindByQuarterAndYearAndIsActiveService.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/service/academicperiod/FindByQuarterAndYearAndIsActiveService.java
@@ -1,4 +1,27 @@
 package io.github.ctorressoftware.academic.enrollment.calendar.application.service.academicperiod;
 
-public class FindByQuarterAndYearAndIsActiveService {
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.findbyquarterandyear.FindByQuarterAndYearAndIsActiveCommand;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.findbyquarterandyear.FindByQuarterAndYearAndIsActiveResult;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.in.academicperiod.findbyquarterandyear.FindByQuarterAndYearAndIsActiveUseCase;
+import io.github.ctorressoftware.academic.enrollment.calendar.application.port.out.AcademicPeriodRepository;
+import io.github.ctorressoftware.academic.enrollment.calendar.domain.model.AcademicPeriod;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindByQuarterAndYearAndIsActiveService implements FindByQuarterAndYearAndIsActiveUseCase {
+
+    private final AcademicPeriodRepository academicPeriodRepository;
+
+    public FindByQuarterAndYearAndIsActiveService(AcademicPeriodRepository academicPeriodRepository) {
+        this.academicPeriodRepository = academicPeriodRepository;
+    }
+
+    @Override
+    public FindByQuarterAndYearAndIsActiveResult find(FindByQuarterAndYearAndIsActiveCommand command) {
+        AcademicPeriod academicPeriod = academicPeriodRepository
+                .findActiveByQuarterAndYear(command.quarter(), command.year())
+                .orElseThrow(() -> new IllegalArgumentException("Academic period not found"));
+
+        return new FindByQuarterAndYearAndIsActiveResult(academicPeriod);
+    }
 }
