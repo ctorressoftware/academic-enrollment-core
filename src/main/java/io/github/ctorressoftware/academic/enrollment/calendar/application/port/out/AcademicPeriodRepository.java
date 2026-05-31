33de85e06055e7145c4bc824510b24fd889ae4f3
Carlos Torres <ctorressoftware@hotmail.com>
2026-05-30 23:46:48 -0400

    feat: add findById method to AcademicPeriodRepository

--- src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/port/out/AcademicPeriodRepository.java
+++ src/main/java/io/github/ctorressoftware/academic/enrollment/calendar/application/port/out/AcademicPeriodRepository.java
@@ -3,9 +3,11 @@ package io.github.ctorressoftware.academic.enrollment.calendar.application.port.
 import io.github.ctorressoftware.academic.enrollment.calendar.domain.model.AcademicPeriod;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface AcademicPeriodRepository {
     AcademicPeriod save(AcademicPeriod academicPeriod);
+    Optional<AcademicPeriod> findById(UUID academicPeriodId);
     Optional<AcademicPeriod> findActiveByQuarterAndYear(
             String quarter,
             short year
